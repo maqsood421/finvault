e415d92e1a75bcc91bb3348552729defaa6aafc4
Maqsood Ahmed <maqsoodnasirani421@gmail.com>
2025-06-16 14:40:27 +0500

    React initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <div className="text-xl font-bold text-blue-600">
+      Tailwind is working!
+    </div>
+  )
+}
+
+export default App
